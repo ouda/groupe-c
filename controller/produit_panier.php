b645ed89c899680b4d1967b4c59b82142a2733ac
reda48fethi <reda.fethi@gmail.com>
2013-12-03 19:46:42 +0000

    Signed-off-by: reda48fethi <reda.fethi@gmail.com>

--- controller/produit_panier.php
+++ controller/produit_panier.php
@@ -3,5 +3,6 @@ include('../lib/db.php');
 include('../model/produit_panier.php');
 $produit=new Produit_Panier();
 $items=$produit->listeProduit();
+echo $items->getId();
 include('../view/html/produit_panier.php');
 ?>
